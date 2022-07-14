017c5f2099852cbfe936ef645370c0e68ea34931
YU KANG JO <46198324+JoYuKang@users.noreply.github.com>
2022-07-14 23:00:27 +0900

    # 시작

---
+++ Hello/Shared/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Shared
+//
+//  Created by Yu kang Jo on 2022/07/14.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
